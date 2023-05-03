d6092aece61c61c6f89194272277a1cdf11c749e
Anton Vaida <vaidaanton.job@gmail.com>
2023-05-03 18:18:16 +0300

    new access token

--- src/services/getCompletedIssue.ts
+++ src/services/getCompletedIssue.ts
@@ -1,5 +1,5 @@
 const PREF_URL = 'https://api.github.com/repos';
-const TOKEN = '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';
+const TOKEN = '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';
 
 export const getCompletedIssues = async (baseURL:string) => {
   let URL = `${PREF_URL}/${baseURL}/issues?state=closed`;
--- src/services/getRepoIform.ts
+++ src/services/getRepoIform.ts
@@ -1,5 +1,5 @@
 const PREF_URL = 'https://api.github.com/repos';
-const TOKEN = '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';
+const TOKEN = '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';
 
 export const getRepoInform = async (baseURL:string) => {
   let URL = `${PREF_URL}/${baseURL}`;
